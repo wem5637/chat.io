51fc8b20463e88f5c90a7048a06164f5b8329420
Will McCracken <wem5637@gmail.com>
2017-07-26 00:34:12 -0500

    deploying

--- rs.js
+++ rs.js
@@ -13,6 +13,6 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(80, function(){
+  console.log('listening on *:default');
 });
